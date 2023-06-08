15649d266f864ab88bd0b1a7c49b27bfeac7f752
unknown <shajibhasan7325@gmail.com>
2023-06-08 06:25:42 +0600

    font page compleate

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -24,7 +24,7 @@ type Props = {};
 const Row1 = (props: Props) => {
   const { palette } = useTheme();
   const { data } = useGetKpisQuery();
-  console.log("data:", data);
+
   const revenue = useMemo(() => {
     return (
       data &&
--- server/index.js
+++ server/index.js
@@ -1,35 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
-import KPI from "./models/KPI.js";
-import { kpis, products } from "./data/data.js";
 import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
 import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
-//  Configuration
-
-const app = express();
+/* CONFIGURATIONS */
 dotenv.config();
+const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-// ROUTES
+/* ROUTES */
 app.use("/kpi", kpiRoutes);
-
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
-// MONGOOSE SETUP
-
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -42,7 +42,7 @@ mongoose
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
-    Product.insertMany(products);
+    // Product.insertMany(products);
     // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
